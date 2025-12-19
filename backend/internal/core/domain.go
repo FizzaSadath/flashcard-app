fdb94d96de18d97b2a89379b743bb2917e997eb8
Fizza Sadath <fizzasadath@gmail.com>
2025-12-19 13:27:05 +0530

    Defines model for cards and their statistics calculated using SM-2 algorithm.

---
+++ backend/internal/core/domain.go
@@ -0,0 +1,26 @@
+package core
+
+import "time"
+
+type Card struct {
+	ID        string
+	DeckID    string
+	Front     string
+	Back      string
+	Stats     CardStats
+	CreatedAt time.Time
+}
+
+type CardStats struct {
+	Repetitions int
+	EaseFactor  float64
+	Interval    int
+}
+
+func InitialStats() CardStats {
+	return CardStats{
+		Repetitions: 0,
+		EaseFactor:  2.5,
+		Interval:    0,
+	}
+}
