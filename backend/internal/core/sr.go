d37a8e32a3089bb8f2fa9892b28da4b9fc504635
Fizza Sadath <fizzasadath@gmail.com>
2025-12-22 16:49:23 +0530

    Implements test case of card statistics when given an incorrect response.

--- backend/internal/core/sr.go
+++ backend/internal/core/sr.go
@@ -4,15 +4,21 @@ import "math"
 
 func CalculateReview(stats CardStats, grade int) CardStats {
 	newStats := stats
-	newStats.Repetitions += 1
 
-	if newStats.Repetitions == 1 {
+	if grade < 3 {
+		newStats.Repetitions = 0
 		newStats.Interval = 1
-	} else if newStats.Repetitions == 2 {
-		newStats.Interval = 6
 	} else {
-		newInterval := float64(newStats.Interval) * newStats.EaseFactor
-		newStats.Interval = int(math.Ceil(newInterval))
+		newStats.Repetitions += 1
+
+		if newStats.Repetitions == 1 {
+			newStats.Interval = 1
+		} else if newStats.Repetitions == 2 {
+			newStats.Interval = 6
+		} else {
+			newInterval := float64(newStats.Interval) * newStats.EaseFactor
+			newStats.Interval = int(math.Ceil(newInterval))
+		}
 	}
 
 	newStats.EaseFactor = stats.EaseFactor + (0.1 - (5.0-float64(grade))*(0.08+(5.0-float64(grade))*0.02))
