931e81b89bf0ded42a4d8004e5d546167d06254a
Fizza Sadath <fizzasadath@gmail.com>
2025-12-22 16:33:27 +0530

    Implements test case of card statisitics when second guess is correct.

--- backend/internal/core/sr.go
+++ backend/internal/core/sr.go
@@ -2,10 +2,12 @@ package core
 
 func CalculateReview(stats CardStats, grade int) CardStats {
 	newStats := stats
-
 	newStats.Repetitions += 1
+
 	if newStats.Repetitions == 1 {
 		newStats.Interval = 1
+	} else if newStats.Repetitions == 2 {
+		newStats.Interval = 6
 	}
 
 	return newStats
