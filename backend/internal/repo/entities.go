df35177c91ba37b473f3f1ea7754cf17d48e40a9
Fizza Sadath <fizzasadath@gmail.com>
2025-12-26 17:12:06 +0530

    Adds database model for card decks

--- backend/internal/repo/entities.go
+++ backend/internal/repo/entities.go
@@ -15,15 +15,30 @@ func (UserEntity) TableName() string {
 	return "users"
 }
 
+type DeckEntity struct {
+	gorm.Model
+
+	UserID uint
+	Name   string
+
+	User UserEntity `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+func (DeckEntity) TableName() string {
+	return "decks"
+}
+
 type CardEntity struct {
 	gorm.Model
 
 	UserID uint       `gorm:"index"`
 	User   UserEntity `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 
-	DeckID uint   `gorm:"index"`
-	Front  string `gorm:"type:text;not null"`
-	Back   string `gorm:"type:text;not null"`
+	DeckID uint       `gorm:"index"`
+	Deck   DeckEntity `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+
+	Front string `gorm:"type:text;not null"`
+	Back  string `gorm:"type:text;not null"`
 
 	Repetitions int     `gorm:"default:0"`
 	EaseFactor  float64 `gorm:"default:2.5"`
